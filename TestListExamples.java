d13519de6cbcaa906da20e1040b7dc3963d1763f
scripe2022 <im@jyh.sb>
2022-11-10 09:18:36 -0800

    nov10 0918

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,15 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import org.hamcrest.*;
+import org.hamcrest.CoreMatchers.*;
 
 public class TestListExamples {
-  // Write your grading tests here!
+    @Test
+    public void testMerge0() {
+        List<String> input1 = Arrays.asList(new String[]{"a", "c", "e"});
+        List<String> input2 = Arrays.asList(new String[]{"b", "d", "f"});
+        List<String> output = ListExamples.merge(input1, input2);
+        List<String> exp = Arrays.asList(new String[]{"a", "b", "c", "d", "e", "f"});
+        assertThat(output, is(exp));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,24 @@
 # Create your grading script here
 
-set -e
+# set -e
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+DIR="submission"
+TESTFILE="TestListExamples.java"
+rm -rf $DIR
+git clone $1 $DIR
+cp $TESTFILE $DIR/
 
-rm -rf student-submission
-git clone $1 student-submission
+cd $DIR
+javac -cp $CPATH *.java 2> compile.error
+if [[ $? -ne 0 ]]; then
+    file_not_found=`grep "file not found" compile.error`
+    if [[ -z "$file_not_found" ]]; then
+        echo "Compile error."
+    else
+        echo "File not found."
+    fi
+    `cat compile.error`
+    exit 1
+fi
 
+java -cp $CPATH org.junit.runner.JUnitCore $TESTFILE
