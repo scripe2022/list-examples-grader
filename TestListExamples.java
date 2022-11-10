fc59ca7d0c6cc0082819c44bcb710081ffec8447
Ruojiang-Zhang <lucasruojiang@gmail.com>
2022-11-10 12:32:54 -0800

    half done lab

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,42 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+    public void testFilter(){
+        List<String> Lst = new ArrayList<>();
+        StringChecker sc = new Checker();
+        Lst.add("abc");
+        Lst.add("xyz");
+        Lst.add("ijk");
+        Lst.add("zab");
+        Lst.add("def");
+        assertArrayEquals(new String[]{"xyz","zab"}, ListExamples.filter(Lst, sc).toArray());
+    }
+
+    @Test
+    public void testMerge(){
+        List<String> Lis1 = new ArrayList<>();
+        List<String> Lis2 = new ArrayList<>();
+        Lis1.add("a");
+        Lis1.add("c");
+        Lis1.add("e");
+        Lis2.add("b");
+        Lis2.add("d");
+        Lis2.add("f");
+        assertArrayEquals(new String[]{"a","b","c","d","e","f"}, ListExamples.merge(Lis1, Lis2).toArray());
+    }
+}
+
+class Checker implements StringChecker{
+  public boolean checkString(String s){
+      if(s.contains("z")){
+          return true;
+      }
+      return false;
+  }
 }
--- grade.sh
+++ grade.sh
@@ -5,3 +5,31 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+if[[ -f student-submission/ListExamples.java]]
+then
+    cp TestListExamples.java student-submission
+    cp -r lib student-submission
+    cp Server.java student-submission
+    cp GradeServer.java student-submission
+else
+    echo "ListExamples.java does not found. Check your submission. "
+    echo "You score is 0. "
+    exit 1
+fi
+
+cd student-submission
+
+set +e
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+
+set -e
+if[[$? == 0]]
+then
+    java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > test-msg.txt
+else
+    echo "compiling error! "
+    echo "You score is 0. "
+    exit 1
+fi
+
+grep -iw "" test-mes.txt
