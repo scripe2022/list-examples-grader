ed41c09c7d5c57c4bc8c38df2ee13630f9f28cee
scripe2022 <im@jyh.sb>
2022-11-15 01:46:10 -0800

    nov15 0146

--- TestListExamples.java
+++ TestListExamples.java
@@ -7,26 +7,35 @@ import java.util.*;
 
 class SChecker implements StringChecker{
     public boolean checkString(String str) {
-        return str.contains("man");
+        return str.contains("united");
     }
 }
 
 public class TestListExamples {
-    @Test
+    @Test(timeout = 100)
     public void testMerge0() {
         List<String> input1 = Arrays.asList(new String[]{"a", "c", "e"});
         List<String> input2 = Arrays.asList(new String[]{"b", "d", "f"});
         List<String> output = ListExamples.merge(input1, input2);
-        assertEquals(output.size(), 6);
-        MatcherAssert.assertThat(output, CoreMatchers.hasItems("a", "b", "c", "d", "e", "f"));
+        List<String> expected = Arrays.asList(new String[]{"a", "b", "c", "d", "e", "f"});
+        Assert.assertArrayEquals(expected.toArray(), output.toArray());
     }
 
-    @Test
+    @Test(timeout = 100)
     public void testFilter0() {
-        List<String> input = Arrays.asList(new String[]{"man united", "man city", "liverpool"});
+        List<String> input = Arrays.asList(new String[]{"milan", "manchester united", "manchester city", "liverpool", "newcastle united", "west ham united"});
+        List<String> expected = Arrays.asList(new String[]{"manchester united", "newcastle united", "west ham united"});
         StringChecker sc = new SChecker();
         List<String> output = ListExamples.filter(input, sc);
-        assertEquals(output.size(), 2);
-        MatcherAssert.assertThat(output, CoreMatchers.hasItems("man united", "man city"));
+        Assert.assertArrayEquals(expected.toArray(), output.toArray());
+    }
+
+    @Test(timeout = 100)
+    public void testFilter1() {
+        List<String> input = Arrays.asList(new String[]{});
+        List<String> expected = Arrays.asList(new String[]{});
+        StringChecker sc = new SChecker();
+        List<String> output = ListExamples.filter(input, sc);
+        Assert.assertArrayEquals(expected.toArray(), output.toArray());
     }
 }
--- grade.sh
+++ grade.sh
@@ -2,21 +2,33 @@ CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 DIR="submission"
 TESTJAVA="TestListExamples.java"
 TESTOBJ="${TESTJAVA%.*}"
-rm -rf $DIR
-git clone $1 $DIR
-cp $TESTJAVA $DIR/
+FILENAME="ListExamples.java"
+COMPILEINFO=".compile.error"
+TESTINFO=".unittest.error"
 
-cd $DIR
-javac -cp $CPATH *.java 2> compile.error
+rm -rf $DIR
+git clone $1 $DIR > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-    file_not_found=`grep "file not found" compile.error`
-    if [[ -z "$file_not_found" ]]; then
-        echo "Compile error."
-    else
-        echo "File not found."
-    fi
-    `cat compile.error`
+    exit
+fi
+cp $TESTJAVA $DIR/
+cd $DIR
+if [[ ! -f $FILENAME ]]; then
+    echo "$FILENAME not found"
+    exit 2
+fi
+javac -cp $CPATH *.java 2> $COMPILEINFO
+if [[ $? -ne 0 ]]; then
+    echo -e "Compile error.\n"
+    cat $COMPILEINFO
     exit 1
 fi
 
-java -cp $CPATH org.junit.runner.JUnitCore $TESTOBJ
+java -cp $CPATH org.junit.runner.JUnitCore $TESTOBJ > $TESTINFO
+
+result=`grep '^[.E]\+$' < .unittest.error`
+total=`grep -o '.' <<<"$result" | grep -c .`
+error=`grep -o 'E' <<<"$result" | grep -c .`
+echo "$((total-error))/$total passed."
+echo -e "\ndetails:"
+cat $TESTINFO
