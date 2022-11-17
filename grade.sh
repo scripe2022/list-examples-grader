7b51138e5ad8c120c6e0915b278ac39d76f67927
scripe2022 <im@jyh.sb>
2022-11-17 02:12:55 -0800

    fix typo

--- grade.sh
+++ grade.sh
@@ -5,7 +5,7 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
-if[[ -f student-submission/ListExamples.java]]
+if [[ -f student-submission/ListExamples.java ]]
 then
     cp TestListExamples.java student-submission
     cp -r lib student-submission
@@ -20,16 +20,16 @@ fi
 cd student-submission
 
 set +e
-javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+javac -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" *.java
 
 set -e
-if[[$? == 0]]
+if [[ $? == 0 ]]
 then
-    java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > test-msg.txt
+    java -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > test-msg.txt
 else
     echo "compiling error! "
     echo "You score is 0. "
     exit 1
 fi
 
-grep -iw "" test-mes.txt
+grep -iw "" test-msg.txt
