0cc7e40a2f944357291542cac285b23b22ac640f
scripe2022 <im@jyh.sb>
2022-11-17 02:53:38 -0800

    suggestion 5

--- grade.sh
+++ grade.sh
@@ -1,34 +1,35 @@
 # Create your grading script here
 
-set -e
-
 rm -rf student-submission
 git clone $1 student-submission
 
-if [[ -f student-submission/ListExamples.java ]]
+if [[ $? -ne 0 ]]
 then
-    cp TestListExamples.java student-submission
-    cp -r lib student-submission
-    cp Server.java student-submission
-    cp GradeServer.java student-submission
-else
+    echo "git clone failed. "
+    exit 2
+fi
+
+if [[ ! -f student-submission/ListExamples.java ]]
+then
     echo "ListExamples.java does not found. Check your submission. "
     echo "You score is 0. "
     exit 1
 fi
+cp TestListExamples.java student-submission
+cp -r lib student-submission
+cp Server.java student-submission
+cp GradeServer.java student-submission
 
 cd student-submission
 
-set +e
 javac -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" *.java
 
-if [[ $? == 0 ]]
+if [[ $? -ne 0 ]]
 then
-    java -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > test-msg.txt
-else
     echo "compiling error! "
     echo "You score is 0. "
     exit 1
 fi
+java -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > test-msg.txt
 
 grep -iw "" test-msg.txt
